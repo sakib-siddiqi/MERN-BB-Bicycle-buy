e7fd144573d304ac0a862353eb69bd4d0922bce6
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-11-14 11:03:01 +0600

    private route for single product

--- src/Pages/Home/Components/HomeBanner.jsx
+++ src/Pages/Home/Components/HomeBanner.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ButtonB from "../../../Shared/Components/Buttons/ButtonB";
 const HomeBanner = () => {
   return (
     <header id="home-header">
       <Container className="center flex-column vh-min-100">
         <h1 className="display-1 fw-bold text-white mb-4">Bicycle Buy</h1>
-        <ButtonB className="px-5">B.B Tour</ButtonB>
+        <Link to="/shop">
+          <ButtonB className="px-5">B.B Tour</ButtonB>
+        </Link>
       </Container>
     </header>
   );
